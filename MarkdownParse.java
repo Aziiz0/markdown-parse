d51a12a932917b6572e8743889dc94dce5ad785e
Aziiz0 <54380763+Aziiz0@users.noreply.github.com>
2022-01-19 19:13:16 -0800

    Update MarkdownParse.java
    If we extend the test-file.md with an extra line it will result in an infinite loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,12 +16,16 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            
             currentIndex = closeParen + 1;
+
+            System.out.println(currentIndex);
+            
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
+		Path fileName = Path.of("test-file.md");//Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
