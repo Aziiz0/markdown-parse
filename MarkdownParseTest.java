6c8b1e19915aabf679b3e874ac94687a0f0b6a9e
Aziiz0 <54380763+Aziiz0@users.noreply.github.com>
2022-01-26 17:54:46 -0800

    Lab4
     Added Lab4 files

---
+++ MarkdownParseTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+	
+	/*@Test2
+	public void addition() {
+		
+	}*/
+}
